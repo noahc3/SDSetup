6ce31608c94b20784ef367a92fe34c90ba5b1844
noahc3 <noahcuroe@gmail.com>
2018-08-16 10:15:59 -0500

    Menu changes

---
+++ SDSetupBlazor/wwwroot/scripts/menu.js
@@ -0,0 +1,11 @@
+﻿window.menu_interop = {
+	interop_registerClick: function () {
+		console.log("fucking haha");
+		$('.ui .item').on('click', function () {
+			console.log("fucking haha numero dos");
+			$('.ui .item').removeClass('active');
+			$(this).addClass('active');
+		});
+		return 1;
+	}
+};
