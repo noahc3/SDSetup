af698cc0fa64c5922cbfa657d343313f99eb2d2c
noahc3 <noahcuroe@gmail.com>
2018-08-18 18:50:45 -0500

    ZIP generation way faster, fully fixing Edge compatibility

--- SDSetupBlazor/wwwroot/scripts/compat.js
+++ SDSetupBlazor/wwwroot/scripts/compat.js
@@ -1,6 +1,6 @@
 ﻿
 function checkIE() {
-	if (window.navigator.userAgent.indexOf('Trident/') > -1) {
+	if (getBrowserCompatInfo() == 2) {
 		$('body').replaceWith(
 			'<div class="ui active dimmer" style="height:100%;width:100%">' +
 			'<img class="ui small image" src="/img/fail.png">' +
@@ -10,17 +10,17 @@ function checkIE() {
 		);
 		return;
 	}
-	if (window.navigator.userAgent.indexOf('MSIE ') > -1) {
-		$('body').html(
-			'<div class="ui active dimmer" style="height:100%;width:100%">' +
-				'<img class="ui small image" src="/img/fail.png">' +
-				'<h3 style="color:#ffffff;margin-top:1.5em">Unfortunately, Internet Explorer is not compatible with this site.</h3>' +
-				'<h6 style="color:#ffffff">(You should seriously consider using a better browser like Firefox or Chrome)</h6>' +
-			'</div>'
-		);
-		return;
-	}
-	return;
 }
 
-
+function getBrowserCompatInfo() {
+	if (window.navigator.userAgent.indexOf("Edge") > -1) {
+		return 1
+	}
+	if (window.navigator.userAgent.indexOf('Trident/') > -1) {
+		return 2
+	}
+	if (window.navigator.userAgent.indexOf('MSIE ') > -1) {
+		return 2
+	}
+	return 0;
+}
