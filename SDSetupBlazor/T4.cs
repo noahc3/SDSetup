49b550d2c502aabe143a28c72ae0a65fd8d21e6d
noahc3 <noahcuroe@gmail.com>
2018-08-24 19:02:22 -0500

    Build date will now auto update

---
+++ SDSetupBlazor/T4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SDSetupBlazor
+{
+    public static class Reference
+    {
+        public static string BuildDate {
+			get {
+				return "2018.08.24.6988ec";
+			}
+		}
+    }
+}
