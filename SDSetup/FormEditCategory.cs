02cfccc88baf7f8100d6d0f557cc963417e3c9f4
noahc3 <noahcuroe@gmail.com>
2018-09-03 12:39:17 -0500

    Manifest Tool 5: Add category support

---
+++ SDSetup/FormEditCategory.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SDSetupManifestGenerator {
+    public partial class FormEditCategory : Form {
+
+        private Platform platform;
+        private PackageSection section;
+        private PackageCategory category;
+
+        public FormEditCategory(Platform platform, PackageSection section) {
+            InitializeComponent();
+
+            this.platform = platform;
+            this.section = section;
+
+            RefreshPlatforms();
+            ddlPlatforms.SelectedItem = platform;
+            RefreshSections();
+            ddlSections.SelectedItem = section;
+        }
+
+        public FormEditCategory(Platform platform, PackageSection section, PackageCategory category) {
+            InitializeComponent();
+
+            this.platform = platform;
+            this.section = section;
+            this.category = category;
+
+            txtId.Text = category.ID;
+            txtName.Text = category.Name;
+            txtDisplayName.Text = category.DisplayName;
+            cbxVisible.Checked = category.Visible;
+
+            RefreshPlatforms();
+            ddlPlatforms.SelectedItem = platform;
+            RefreshSections();
+            ddlSections.SelectedItem = section;
+
+            foreach (string k in category.When) lvwWhenDependencies.Items.Add(k);
+        }
+
+        private void RefreshPlatforms() {
+            ddlPlatforms.Items.Clear();
+            foreach (Platform k in G.manifest.Platforms.Values) ddlPlatforms.Items.Add(k);
+        }
+
+        private void RefreshSections() {
+            ddlSections.Items.Clear();
+            if (((Platform)ddlPlatforms.SelectedItem) == null) return;
+            foreach (PackageSection k in ((Platform)ddlPlatforms.SelectedItem).PackageSections) ddlSections.Items.Add(k);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            if ((PackageSection)ddlSections.SelectedItem == null) {
+                MessageBox.Show("Please select a section!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (category == null) {
+                bool exists = false;
+                foreach (PackageCategory k in ((PackageSection)ddlSections.SelectedItem).Categories) {
+                    if (k.ID == txtId.Text) {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (exists) {
+                    MessageBox.Show("A category with that ID already exists in that section!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                List<string> whens = new List<string>();
+                foreach (ListViewItem k in lvwWhenDependencies.Items) whens.Add(k.Text);
+
+                ((PackageSection)ddlSections.SelectedItem).Categories.Add(new PackageCategory(txtId.Text, txtName.Text, txtDisplayName.Text, cbxVisible.Checked, whens, (int) nudWhenMode.Value, new List<PackageSubcategory>()));
+            } else {
+                bool exists = false;
+                foreach (PackageCategory k in ((PackageSection)ddlSections.SelectedItem).Categories) {
+                    if (k.ID == txtId.Text) {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (exists) {
+                    if (txtId.Text != category.ID) {
+                        MessageBox.Show("A category with that ID already exists in that section!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (txtId.Text == category.ID && (PackageSection)ddlSections.SelectedItem != section) {
+                        MessageBox.Show("A category with that ID already exists in that section!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+                if ((PackageSection)ddlSections.SelectedItem != section) {
+                    section.Categories.Remove(category);
+                    ((PackageSection)ddlSections.SelectedItem).Categories.Add(category);
+                }
+
+                category.ID = txtId.Text;
+                category.Name = txtName.Text;
+                category.DisplayName = txtDisplayName.Text;
+                category.Visible = cbxVisible.Checked;
+
+                List<string> whens = new List<string>();
+                foreach (ListViewItem k in lvwWhenDependencies.Items) whens.Add(k.Text);
+
+                category.When = whens;
+            }
+
+            Close();
+        }
+
+        private void btnAddWhen_Click(object sender, EventArgs e) {
+            if (String.IsNullOrEmpty(txtWhen.Text) || String.IsNullOrWhiteSpace(txtWhen.Text)) {
+                MessageBox.Show("Please enter a package name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool exists = false;
+            foreach (ListViewItem k in lvwWhenDependencies.Items) {
+                if (k.Text == txtWhen.Text) {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (exists) {
+                MessageBox.Show("That package is already a when dependency!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            lvwWhenDependencies.Items.Add(new ListViewItem(txtWhen.Text));
+            txtWhen.Text = "";
+        }
+
+        private void btnDeleteWhen_Click(object sender, EventArgs e) {
+            if (lvwWhenDependencies.SelectedItems.Count < 1) return;
+            lvwWhenDependencies.Items.Remove(lvwWhenDependencies.SelectedItems[0]);
+        }
+
+        private void ddlPlatforms_SelectedValueChanged(object sender, EventArgs e) {
+            RefreshSections();
+        }
+    }
+}
---
+++ SDSetup/FormViewCategories.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SDSetupManifestGenerator {
+    partial class FormViewCategories {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ddlPlatforms = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lvwCategories = new System.Windows.Forms.ListView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlSections = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Platform";
+            // 
+            // ddlPlatforms
+            // 
+            this.ddlPlatforms.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlPlatforms.FormattingEnabled = true;
+            this.ddlPlatforms.Location = new System.Drawing.Point(12, 25);
+            this.ddlPlatforms.Name = "ddlPlatforms";
+            this.ddlPlatforms.Size = new System.Drawing.Size(189, 21);
+            this.ddlPlatforms.TabIndex = 0;
+            this.ddlPlatforms.SelectedValueChanged += new System.EventHandler(this.ddlPlatforms_SelectedValueChanged);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(12, 464);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(189, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lvwCategories
+            // 
+            this.lvwCategories.Location = new System.Drawing.Point(12, 92);
+            this.lvwCategories.Name = "lvwCategories";
+            this.lvwCategories.Size = new System.Drawing.Size(189, 264);
+            this.lvwCategories.TabIndex = 2;
+            this.lvwCategories.UseCompatibleStateImageBehavior = false;
+            this.lvwCategories.View = System.Windows.Forms.View.List;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 420);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(189, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(12, 391);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(189, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 362);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(189, 23);
+            this.btnNew.TabIndex = 3;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Section";
+            // 
+            // ddlSections
+            // 
+            this.ddlSections.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlSections.FormattingEnabled = true;
+            this.ddlSections.Location = new System.Drawing.Point(12, 65);
+            this.ddlSections.Name = "ddlSections";
+            this.ddlSections.Size = new System.Drawing.Size(189, 21);
+            this.ddlSections.TabIndex = 1;
+            this.ddlSections.SelectedValueChanged += new System.EventHandler(this.ddlSections_SelectedValueChanged);
+            // 
+            // FormViewCategories
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 503);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ddlSections);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddlPlatforms);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lvwCategories);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNew);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormViewCategories";
+            this.Text = "FormViewCategories";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ddlPlatforms;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListView lvwCategories;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlSections;
+    }
+}
