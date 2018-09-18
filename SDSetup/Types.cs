aaaa2d6081bc2208069d50f121720790c36f8dd9
noahc3 <noahcuroe@gmail.com>
2018-09-18 12:24:06 -0500

    Start working on preconfigured bundles

--- SDSetup/Types.cs
+++ SDSetup/Types.cs
@@ -48,23 +48,25 @@ namespace SDSetupManifestGenerator {
         public string ID = "";
         public string Color = "";
         public bool Visible = true;
+        public List<Bundle> Bundles = new List<Bundle>();
         public List<PackageSection> PackageSections = new List<PackageSection>();
+        
 
-        public Platform(string name, string menuName, string homeIcon, string iD, List<PackageSection> packageSections, string color, bool Visible) {
-            Name = name;
-            MenuName = menuName;
-            HomeIcon = homeIcon;
-            ID = iD;
-            PackageSections = packageSections;
-            Color = color;
-        }
+
 
         public Platform() {
 
         }
 
-        public override string ToString() {
-            return Name;
+        public Platform(string name, string menuName, string homeIcon, string iD, string color, bool visible, List<PackageSection> packageSections, List<Bundle> bundles) {
+            Name = name;
+            MenuName = menuName;
+            HomeIcon = homeIcon;
+            ID = iD;
+            Color = color;
+            Visible = visible;
+            PackageSections = packageSections;
+            Bundles = bundles;
         }
     }
 
@@ -76,7 +78,7 @@ namespace SDSetupManifestGenerator {
         public bool Visible = true;
         public List<string> When = new List<string>();
         public int WhenMode = 0; //0: all | 1: any
-        public string Footer = "";
+        public string Footer;
         public List<PackageCategory> Categories = new List<PackageCategory>();
 
 
@@ -95,10 +97,6 @@ namespace SDSetupManifestGenerator {
             WhenMode = whenMode;
             Categories = categories;
             Footer = footer;
-        }
-
-        public override string ToString() {
-            return Name;
         }
     }
 
@@ -126,10 +124,6 @@ namespace SDSetupManifestGenerator {
             WhenMode = whenMode;
             Subcategories = subcategories;
         }
-
-        public override string ToString() {
-            return Name;
-        }
     }
 
     public class PackageSubcategory {
@@ -154,10 +148,6 @@ namespace SDSetupManifestGenerator {
             When = when;
             WhenMode = whenMode;
             Packages = packages;
-        }
-
-        public override string ToString() {
-            return Name;
         }
     }
 
@@ -199,10 +189,6 @@ namespace SDSetupManifestGenerator {
             Dependencies = dependencies;
             Artifacts = artifacts;
         }
-
-        public override string ToString() {
-            return Name;
-        }
     }
 
     public class Artifact {
@@ -219,6 +205,22 @@ namespace SDSetupManifestGenerator {
         }
 
         public Artifact() {
+
+        }
+    }
+
+    public class Bundle {
+        public string Name;
+        public string Description;
+        public List<string> Packages;
+
+        public Bundle(string name, string description, List<string> packages) {
+            Name = name;
+            Description = description;
+            this.Packages = packages;
+        }
+
+        public Bundle() {
 
         }
     }
