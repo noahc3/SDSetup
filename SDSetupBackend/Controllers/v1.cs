2310fe81778eef6275a1649a276f9925d50e6cb7
znxDomain <znxDomain@gmail.com>
2019-05-22 07:32:33 -0600

    Revert "Increase Backend Zip Stream Timeout"
    This reverts commit 7fa8cff5739e5813316b0c10bdab4d9b3aceae2f.

--- SDSetupBackend/Controllers/v1.cs
+++ SDSetupBackend/Controllers/v1.cs
@@ -86,7 +86,7 @@ namespace SDSetupBackend.Controllers {
                     DeletingFileStream stream = (DeletingFileStream) ZipFromFilestreams(files.ToArray(), uuid);
 
                     Program.generatedZips[uuid] = stream;
-                    stream.Timeout(180000);
+                    stream.Timeout(30000);
 
                     Program.uuidLocks.Remove(uuid);
                     return new ObjectResult("READY");
