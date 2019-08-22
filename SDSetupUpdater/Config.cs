f41c38247c1c4f6291932b730d0bee3b0629a67f
Noah <noahcuroe@gmail.com>
2019-08-22 11:23:59 -0500

    Auto updating packages.

---
+++ SDSetupUpdater/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDSetupUpdater {
+    class Config {
+        public string GithubUsername { get; set; }
+        public string GithubAuthToken { get; set; }
+        public string WebhookEndpoint { get; set; }
+        public string BackendHostname { get; set; }
+        public string LibGetRepo { get; set; }
+        public string KosmosUpdaterScriptPath { get; set; }
+        public long KosmosRepositoryId { get; set; }
+    }
+}
