85a3a0b137439cc4526a2b63e81ace8c48cfe952
noahc3 <noahc3@users.noreply.github.com>
2020-04-19 13:30:40 -0500

    Support new build script (#160)

--- SDSetupUpdater/Config.cs
+++ SDSetupUpdater/Config.cs
@@ -6,10 +6,12 @@ namespace SDSetupUpdater {
     class Config {
         public string GithubUsername { get; set; }
         public string GithubAuthToken { get; set; }
+        public string GitlabAuthToken { get; set; }
         public string WebhookEndpoint { get; set; }
         public string BackendHostname { get; set; }
         public string LibGetRepo { get; set; }
-        public string KosmosUpdaterScriptPath { get; set; }
+        public string KosmosScriptPath { get; set; }
+        public string KosmosUpdaterBuilderSubdirectory { get; set; }
         public long KosmosRepositoryId { get; set; }
         public string KosmosMasterUrl { get; set; }
         public List<string> OldPackagesets { get; set; }
