7d26318c85b2025b006c1ee75e562f2142b7a1ec
Noah <noahcuroe@gmail.com>
2021-07-20 12:09:44 -0500

    Update to Blazor WASM 3.2.0

--- SDSetupBlazor/Program.cs
+++ SDSetupBlazor/Program.cs
@@ -2,20 +2,27 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-using Microsoft.AspNetCore.Blazor.Hosting;
+using BlazorStrap;
+using Cloudcrate.AspNetCore.Blazor.Browser.Storage;
+using CurrieTechnologies.Razor.Clipboard;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SDSetupBlazor
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+    public class Program {
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddStorage();
+            builder.Services.AddBootstrapCSS();
+            builder.Services.AddClipboard();
 
-            CreateHostBuilder(args).Build().Run();
+            await builder.Build().RunAsync();
         }
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
     }
 }
